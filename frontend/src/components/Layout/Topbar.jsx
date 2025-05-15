9f853512a8b2f8506ea035537a0a5c712854daff
Ajai-3 <ajaipjayan@gmail.com>
2025-05-15 08:25:14 +0530

    topbar completed

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { TbBrandMeta } from "react-icons/tb";
+import { RiTwitterXLine } from "react-icons/ri";
+import { IoLogoInstagram } from "react-icons/io5";
 
 const Topbar = () => {
   return (
-    <div className='bg-[#35A29F]'>Topbar</div>
-  )
-}
+    <div className="bg-rabbit-green text-white">
+      <div className="container mx-auto flex justify-between items-center py-2 px-4 sm:px-8 md:px-12 lg:px-16">
 
-export default Topbar
+        {/* Social Media Icons */}
+        <div className="hidden md:flex items-center space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className="h-6 w-6" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className="h-5 w-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <RiTwitterXLine className="h-5 w-5" />
+          </a>
+        </div>
+
+        {/* Centered Text */}
+        <div className="text-sm text-center flex-grow">
+           <span>We ship worldwide - Fast and relible shipping!</span>
+        </div>
+
+        {/* Phone Number */}
+        <div className="text-sm hidden md:block">
+          <a href="tel:+1234567890" className="hover:text-gray-300">+1 (234) 567-890 </a>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rabbit-green": "#309692"
+      },
+    },
   },
   plugins: [],
 }
